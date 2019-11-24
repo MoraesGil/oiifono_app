f11647ef95a31c75408f94af1be45d8f8f07def5
gilberto moraes <>
2019-11-24 13:45:15 -0300

    add content type api and option menu package

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
  
-console.log('call')
 const api = axios.create({
   baseUrl: "http://10.9.8.16:3000/api",
   timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" }
+  headers: {
+    Accept: "application/json",
+   "Content-Type": "application/json"
+  }
 });
 
 export default api;
