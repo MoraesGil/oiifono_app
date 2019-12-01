305936f1c51beba471f4f519a26bacd93944ea50
gilberto moraes <>
2019-12-01 16:05:13 -0300

    add new colors and reorganized colors file

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -7,13 +7,32 @@ export default {
   tabBar: "#fefefe",
   errorBackground: "red",
   errorText: "#fff",
-  warningBackground: "#EAEB5E",
-  warningText: "#666804",
+
   noticeBackground: tintColor,
   noticeText: "#fff",
   female: "#cc317c",
   male: "#517fa4",
   disability: "#3b3e91",
   ok: "#0f0",
-  danger: "#f00"
+  danger: "#f00",
+
+  primaryBackground: "#cce5ff",
+  primaryText: "#004085",
+  primaryBorder: "#b8daff",
+
+  dangerBackground: "#f8d7da",
+  dangerText: "#721c24",
+  dangerBorder: "#f5c6cb",
+
+  warningBackground: "#fff3cd",
+  warningText: "#856404",
+  warningBorder: "#ffeeba",
+
+  infoBackground: "#d1ecf1",
+  infoText: "#0c5460",
+  infoBorder: "#bee5eb",
+
+  successBackground: "#d4edda",
+  successText: "#155724",
+  successBorder: "#c3e6cb"
 };
